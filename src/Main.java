1b75e64b8fa2a76ad67c70661ae8296d14bb0386
d1305 <d1305.com@gmail.com>
2020-10-16 22:39:58 +0500

    update file
    changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-
-
 public class Main {
     public static void main(String[] args) throws Exception {
         int result = 0;
@@ -8,22 +6,15 @@ public class Main {
         int op1, op2 = 0;
         System.out.println("Input");
         Scanner scanner = new Scanner(System.in);
-
-        boolean isExit = false;
-        while (!isExit) {
-            String[] data = scanner.nextLine().replaceAll("\\s+", "").split("");
+        String calculation = scanner.nextLine();
+        String[] data = calculation.split(" ");
             String input1 = data[0];
             String operator = data[1];
             String input2 = data[2];
-            //String input1 = scanner.nextLine();
-            //String operator = scanner.nextLine();
-            //String input2 = scanner.nextLine();
-
             System.out.println("Output");
             input1 = input1.toUpperCase();
             input2 = input2.toUpperCase();
             Validate validate = new Validate();
-            // validate.validateInput(input1, input2, operator)[0]
             if (validate.validateInput(input1, operator, input2) == ("roman")) {
                 ConvertRoman romanOp = new ConvertRoman();
                 op1 = romanOp.romanToInt(input1);
@@ -65,7 +56,5 @@ public class Main {
 
             System.out.println(outputStr);
 
-            return;
-        }
     }
-}
+    }
